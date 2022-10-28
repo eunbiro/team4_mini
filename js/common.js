a14eeef8876ee69470b824e4537af5619a2275b9
github eunbiro <github ebee91@naver.com>
2022-10-28 14:37:15 +0900

    up

--- js/common.js
+++ js/common.js
@@ -4,9 +4,12 @@ const $btnUp = document.querySelector(".button__up");
 const $btnDown = document.querySelector(".button__down");
 
 $btnUp.onclick = () => {
-  window.scrollTo({top: 0, behavior: "smooth"});
+  $('html,body').animate({ scrollTop: 0 }, 500);
+  // window.scrollTo({top: 0, behavior: "smooth"});
+  console.log(1)
 }
 
-$btnDown.click = () => {
+$btnDown.onclick = () => {
 	$("html, body").animate({ scrollTop: $(document).height() }, 500);
+  console.log(2)
 };
