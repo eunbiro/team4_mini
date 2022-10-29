0ac30fa266c22c6ef33a4b324ecaa07010bddd4f
github eunbiro <github ebee91@naver.com>
2022-10-29 22:43:17 +0900

    up

--- js/main.js
+++ js/main.js
@@ -1,16 +1,14 @@
-ov_text
-ov_poster
 
-/* 1. PC버전 서브메뉴 관련 */
-const ovPoster = document.querySelector('.ov_poster')
-const ovText = document.querySelectorAll('.ov_text')
-
-// 마우스 오버 시 하위 메뉴 보이기
-ovPoster.addEventListener('mouseover', () => {
-    ovText.style.display = 'block';                            // 마우스 오버시 panel 나타냄
-});
-
-// 마우스 치울 때 하위 메뉴 숨기기
-ovPoster.addEventListener('mouseout', () => {
-    ovText.style.display = 'none';                             // 마우스 아웃 시 panel 숨기기
-});
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    autoplay: {
+      delay: 2500,
+    },
+    speed: 800
+  });
