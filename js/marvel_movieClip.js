31d253b7b62e271d984200245639525d263f62c2
github eunbiro <github ebee91@naver.com>
2022-11-01 00:25:40 +0900

    탭메뉴수정_이미지주변글씨수정

--- js/marvel_movieClip.js
+++ js/marvel_movieClip.js
@@ -1,51 +1,34 @@
-// 모바일 햄버거 버튼 클릭 시
+// 탭메뉴 클릭 시
 $(".menu__still").click(function() {
-  $(".sub__movieclip").hide()       // display : none
-  $(".sub__artwork").hide()       // display : none
-  $(".sub__stillshot").show()           // display : block
+  $(".main__movieclip").hide()            // display : none
+  $(".main__artwork").hide()              // display : none
+  $(".main__stillshot").show()            // display : block
+  $(".menu__still").css("border-bottom", "0")
+  $(".menu__clip").css("border-bottom", "1px solid #666")
+  $(".menu__art").css("border-bottom", "1px solid #666")
 
-  // $("#clip__content").empty()           // #mobile_menu 하위 태그 초기화
-
-  // var nav = $(".main__stillshot").clone()
-
-  // $("#clip__content").append(nav)       // append : 선택한 요소 하위 태그로 삽입
-  // $("#clip__content").show()
 })
 
 $(".menu__clip").click(function() {
-  $(".sub__stillshot").hide()
-  $(".sub__artwork").hide()       // display : none
-  $(".sub__movieclip").show()
-
-  // $("#clip__content").empty()           // #mobile_menu 하위 태그 초기화
-
-  // var nav = $(".main__movieclip").clone()
-
-  // $("#clip__content").append(nav)       // append : 선택한 요소 하위 태그로 삽입
-  // $("#clip__content").show()
+  $(".main__stillshot").hide()
+  $(".main__artwork").hide()
+  $(".main__movieclip").show()
+  $(".menu__clip").css("border-bottom", "0")
+  $(".menu__still").css("border-bottom", "1px solid #666")
+  $(".menu__art").css("border-bottom", "1px solid #666")
 })
 
 $(".menu__art").click(function() {
-  $(".sub__stillshot").hide()
-  $(".sub__movieclip").hide()       // display : none
-  $(".sub__artwork").show()
-
-  // $("#clip__content .con").empty()           // #mobile_menu 하위 태그 초기화
-
-  // var nav = $(".main__artwork").clone()
-
-  // $("#clip__content").append(nav)       // append : 선택한 요소 하위 태그로 삽입
-  // $("#clip__content").show()
+  $(".main__stillshot").hide()
+  $(".main__movieclip").hide()
+  $(".main__artwork").show()
+  $(".menu__art").css("border-bottom", "0")
+  $(".menu__clip").css("border-bottom", "1px solid #666")
+  $(".menu__still").css("border-bottom", "1px solid #666")
 })
 
-// const tabList = document.querySelectorAll(".clip__menu .menu__list > li");
-
-// for (var i = 0; tabList.length; i++) {
-//   tabList[i].querySelector(".btn").addEventListener('click', function(e) {
-//     e.preventDefault();
-//     for (var j = 0; j < tabList.length; j++) {
-//       tabList[j].classList.remove('tab__on');
-//     }
-//     this.parentNode.classList.add('tab__on')
-//   });
-// }
+$(document).ready(function () {
+  $(".ov_poster img").hover(function () {
+    $(".ov_poster p").css("opacity", "1")
+  })
+})
