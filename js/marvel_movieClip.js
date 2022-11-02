e89b660f2b66aac854da2483ca073bee578948d3
github eunbiro <github ebee91@naver.com>
2022-11-02 17:22:35 +0900

    이미지 클릭시 팝업수정

--- js/marvel_movieClip.js
+++ js/marvel_movieClip.js
@@ -1,28 +1,35 @@
 // 탭메뉴 클릭 시
-$(".menu__still").click(function() {
-  $(".main__movieclip").hide()            // display : none
-  $(".main__artwork").hide()              // display : none
-  $(".main__stillshot").show()            // display : block
-  $(".menu__still").css("border-bottom", "0")
-  $(".menu__clip").css("border-bottom", "1px solid #666")
-  $(".menu__art").css("border-bottom", "1px solid #666")
+import { children } from 'dom7/dist/dom7.modular';
+$('.menu__still').click(function () {
+  $('.main__movieclip').hide(); // display : none
+  $('.main__artwork').hide(); // display : none
+  $('.main__stillshot').show(); // display : block
+  $('.menu__still').css('border-bottom', '0');
+  $('.menu__clip').css('border-bottom', '1px solid #666');
+  $('.menu__art').css('border-bottom', '1px solid #666');
+});
 
-})
+$('.menu__clip').click(function () {
+  $('.main__stillshot').hide();
+  $('.main__artwork').hide();
+  $('.main__movieclip').show();
+  $('.menu__clip').css('border-bottom', '0');
+  $('.menu__still').css('border-bottom', '1px solid #666');
+  $('.menu__art').css('border-bottom', '1px solid #666');
+});
 
-$(".menu__clip").click(function() {
-  $(".main__stillshot").hide()
-  $(".main__artwork").hide()
-  $(".main__movieclip").show()
-  $(".menu__clip").css("border-bottom", "0")
-  $(".menu__still").css("border-bottom", "1px solid #666")
-  $(".menu__art").css("border-bottom", "1px solid #666")
-})
+$('.menu__art').click(function () {
+  $('.main__stillshot').hide();
+  $('.main__movieclip').hide();
+  $('.main__artwork').show();
+  $('.menu__art').css('border-bottom', '0');
+  $('.menu__clip').css('border-bottom', '1px solid #666');
+  $('.menu__still').css('border-bottom', '1px solid #666');
+});
 
-$(".menu__art").click(function() {
-  $(".main__stillshot").hide()
-  $(".main__movieclip").hide()
-  $(".main__artwork").show()
-  $(".menu__art").css("border-bottom", "0")
-  $(".menu__clip").css("border-bottom", "1px solid #666")
-  $(".menu__still").css("border-bottom", "1px solid #666")
-})
+lightbox.option({
+  resizeDuration: 200,
+  wrapAround: true,
+  disableScrolling: false,
+  fitImagesInViewport: false,
+});
